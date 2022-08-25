b813bbd042b1fd70de95ff914d6e7ee929b5d82a
German Gabriel Velardez <germandev@gmail.com>
2022-08-25 13:49:18 -0300

    More commands addded

--- Core/Src/AT45DB041.c
+++ Core/Src/AT45DB041.c
@@ -19,35 +19,60 @@
 
 
 
-#define      CMD_READSTATUS        (0x57)
+#define      CMD_READSTATUS                  (0xD7)    //! status register 2 bytes
 
 
 // READ FUNCTIONS
-#define      CMD_READBUFF1              (0x54)
-#define      CMD_READBUFF2              (0x56)
-#define      CMD_READPAGE               (0x52)      
+#define      CMD_READBUFF1                   (0x54)
+#define      CMD_READBUFF2                   (0x56)
+#define      CMD_READPAGE                    (0x52)      
 
 
 //WRITE FUNCTIONS
+#define    CMD_WRITEBUFF1                    (0x84)
+#define    CMD_WRITEBUFF2                    (0x87)
+#define    CMD_PAGETOBUFF1                   (0x53)
+#define    CMD_PAGETOBUFF2                   (0x55)
 
-#define    CMD_WRITEBUFF1                (0x84)
-#define    CMD_WRITEBUFF2                (0x87)
-#define    CMD_WRITEPAGEWBUFF1           (0X82)
-#define    CMD_WRITEPAGEWBUFF2           (0X85)
+#define    CMD_PAGEERASEBUFF1                 (0x83)
+#define    CMD_PAGEERASEBUFF2                 (0X86)
 
+#define    CMD_PAGEBUFF1                      (0X88)
+#define    CMD_PAGEBUFF1                      (0X89)
 
+#define    CMD_PAGEBUFF1POS                    (0x02) // ! Permite seleecionar la posicion inicial
+
+// COMPARE FUNCTIONS
+#define    CMD_CMPBUFF1                      (0x60)
+#define    CMD_CMPBUFF2                      (0x61)
 
 // STATE
+#define CMD_GETSTATUS                      (0xD7)
+#define CMD_RESUMEN                        (0xAB)
+#define CMD_LOWPOWER                       (0xB9)  
 
-#define CMD_RESUMEN                       (0xAB)
-#define CMD_LOWPOWER                      (0x9B)  // INTO LOW POWER
 
+// CONFIG SIZE FROM 265 TO 255 BYTE PER PAGE
+#define CMD_RESIZE                       (0x3D, 0x2A, 0x80, 0xA6)
+#define CMD_READSTATUS                   (0x57)
+
+// ERASE MEMORY
+
+#define CMD_ERASEPAGE                   (0X81)
+#define CMD_ERASEBLOCK                  (0x50)
+#define CMD_ERASESECTOR                 (0x7C)
+#define SEC_ERASECHIP                   (0xC7, 0x94, 0x80, 0x9A)
 
 
 // MASK
 
 #define AT45DB_STATUS_COMP                (1 << 6) /* COMP */
 #define AT45DB_STATUS_READY               (1 << 7) /* RDY/BUSY */
+
+
+
+
+
 
 #define PRIVATE    static
 
@@ -91,3 +116,11 @@ PRIVATE  status_t  spi_read(uint8_t* buffer, size_t len){
 
 
 
+
+
+// PRIVATE FUNCTION API
+
+
+PRIVATE uint8_t  at45db_status(){
+
+}
